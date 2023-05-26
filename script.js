e72e42a9c4192c51bf8786bfbe28fd90ee2ff2b8
KimIcexy <kimicexy2002@gmail.com>
2023-05-26 20:13:14 +0700

    edge detection

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 var canvas = document.getElementById('canvas');
-var imageInput = document.getElementById('imageInput');
-const maxHeight = 600, maxWidth = 800
-const downloadBtn = document.getElementById('downloadBtn');
+var context = canvas.getContext('2d');
+var uploadBtn = document.getElementById('imageInput');
+const maxHeight = 600, maxWidth = 800;
+var downloadBtn = document.getElementById('downloadBtn');
+var edgeBtn = document.getElementById('edgeBtn');
+
 
 // Lắng nghe sự kiện khi tải ảnh lên
-imageInput.addEventListener('change', function (e) {
+uploadBtn.addEventListener('change', function (e) {
     var file = e.target.files[0];
     var reader = new FileReader();
 
@@ -48,3 +51,58 @@ downloadBtn.addEventListener("click", function () {
     a.download = "edited-image.png";
     a.click();
 });
+
+// Lọc biên cạnh
+edgeBtn.addEventListener("click", function () {
+    // Lấy dữ liệu hình ảnh từ canvas
+    var img = context.getImageData(0, 0, canvas.width, canvas.height);
+    // Phát hiện biên cạnh của ảnh
+    var edgeImageData = new ImageData(detectEdges(img), canvas.width, canvas.height);
+    context.putImageData(edgeImageData, 0, 0);
+});
+
+// Phát hiện biên cạnh của ảnh
+function detectEdges(img) {
+    const data = img.data;
+    const width = img.width;
+    const height = img.height;
+
+    // Chuyển sang ảnh xám
+    const grayscale = new Uint8ClampedArray(width * height * 4);
+    for (let i = 0; i < data.length; i += 4) {
+        avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+        for (let k = 0; k < 3; k++)
+            grayscale[i + k] = avg;
+        grayscale[i + 3] = 255
+    }
+
+    // Áp dụng bộ lọc Sobel để phát hiện biên cạnh
+    const result = new Uint8ClampedArray(width * height * 4);
+    const sobelX = [-1, 0, 1, -2, 0, 2, -1, 0, 1];
+    const sobelY = [-1, -2, -1, 0, 0, 0, 1, 2, 1];
+    const step_y = width * 4;
+
+    for (let x = 1; x < width - 1; x++) {
+        for (let y = 1; y < height - 1; y++) {
+            let sumX = 0;
+            let sumY = 0;
+            // Nhân tích chập với bộ lọc
+            for (let kx = -1; kx <= 1; kx++) {
+                for (let ky = -1; ky <= 1; ky++) {
+                    const row = y + ky;
+                    const col = x + kx;
+                    let idx = row * step_y + col * 4;
+                    sumX += grayscale[idx] * sobelX[(kx + 1) * 3 + ky + 1];
+                    sumY += grayscale[idx] * sobelY[(kx + 1) * 3 + ky + 1];
+                }
+            }
+            // Lưu kết quả vào result
+            const magnitude = Math.sqrt(sumX * sumX + sumY * sumY);
+            let idx = y * step_y + x * 4;
+            for (let k = 0; k < 3; k++)
+                result[idx + k] = magnitude;
+            result[idx + 3] = 255;
+        }
+    }
+    return result;
+}
